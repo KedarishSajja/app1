3b8967a17b6bf1b751aa9340f23bb5676e7da1ec
Shlomi Nissan <contact@snissan.me>
2015-03-12 15:07:01 +1100

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+
+$url = $_GET['url'];
+ 
+$urlArray = array();
+$urlArray = explode("/",$url);
+
+$page = $urlArray[0];
+
+// Include the header
+include( ROOT . DS . 'views' . DS . 'header.php' );
+
+// Include the page content
+if( file_exists( ROOT . DS . 'views' . DS . $page . '.php') ) {
+
+	include( ROOT . DS . 'views' . DS . $page . '.php' );
+
+} else {
+
+	include( ROOT . DS . 'views' . DS . 'home.php' );
+
+}
+
+// Include the footer
+include( ROOT . DS . 'views' . DS . 'footer.php' );
